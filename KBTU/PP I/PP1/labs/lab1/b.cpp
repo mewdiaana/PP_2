b0dd37b12b24a6c4d33368ab36bdd2381c0391d6
Marzabayeva Diana <156778435+mewdiaana@users.noreply.github.com>
2024-04-17 01:11:10 +0500

    Delete KBTU directory

--- KBTU/PP I/PP1/labs/lab1/b.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <math.h>
-using namespace std;
-int main(){
-    double a;
-    cin >>a;
-    cout << sqrt(a);
-}
--- KBTU/PP I/PP1/labs/lab3/m(dn).cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int a, sum=0;
-    while (cin>>a){
-        sum=a+sum;
-    }
-    cout << sum;
-}
